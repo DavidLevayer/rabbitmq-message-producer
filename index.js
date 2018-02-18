fa73a2fa6cb931d361cfd4d79e0f6ca410e09cc2
DavidLevayer <david.levayer@gmail.com>
2018-02-18 11:00:24 +0100

    Add message configuration

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
+const config = require('./config');
 
-amqp.connect('amqp://localhost', function(err, conn) {
+const serverUrl = `${config.rabbitmq.protocol}://${config.rabbitmq.hostname}`;
+
+amqp.connect(serverUrl, function(err, conn) {
     conn.createChannel(function(err, ch) {
-        var q = 'hello';
-        var msg = 'Hello World!';
 
-        ch.assertQueue(q, {durable: false});
-        // Note: on Node 6 Buffer.from(msg) should be used
-        ch.sendToQueue(q, new Buffer(msg));
-        console.log(" [x] Sent %s", msg);
+        const exchangeName = config.rabbitmq.exchangeName;
+        const routingKey = config.rabbitmq.routingKey;
+
+        const messageTemplate = config.message.template;
+        const values = config.message.values;
+
+        ch.assertExchange(exchangeName, 'direct', config.rabbitmq.options);
+
+        for(let i=0; i<config.message.number; i++) {
+            const msg = injectArgs(messageTemplate, values);
+            ch.publish(exchangeName, routingKey, new Buffer(msg));
+            console.log("%d - [x] Sent %s", i, msg);
+        }
     });
+
     setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
+
+const injectArgs = function(rawTemplate, values) {
+  let pattern = /%([0-9]+)%/;
+
+  return rawTemplate.replace(pattern, function(a, match) {
+      const value = values[match];
+
+      let replacement = '';
+      if(typeof value === 'object') {
+          replacement = getRandomInt(value);
+      }
+
+      return replacement;
+  });
+};
+
+const getRandomInt = function(param) {
+    const min = param.min;
+    const max = param.max;
+
+    return Math.floor(Math.random() * (max - min)) + min;
+};
